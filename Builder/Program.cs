15818d3cf9341def868f584d7a0597c697f220ca
William Sanders <william.sanders076@gmail.com>
2017-08-05 16:50:24 -0400

    Delete enum of characters and add method to extract characters from assembly.

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,46 @@ namespace Builder
     {
         static void Main(string[] args)
         {
-            var x = new EccentricCharacterBuilder();
-            var character = x.GetCharacter();
+            var keepLooping = true;
+
+            Console.WriteLine("**********************************************************************************************************");
+            Console.WriteLine("                  WELCOME TO THE CHARACTER BUILDER PROGRAM -- WHICH IS A PRETTY NEAT PROGRAM!");
+            Console.WriteLine("**********************************************************************************************************\n");
+
+            while (keepLooping)
+            {
+                Console.WriteLine("Enter the number of the character that you want to build.");
+
+                PrintCharacters();
+
+                keepLooping = KeepGoing();
+            }            
+        }
+
+        static void PrintCharacters()
+        {
+            var characters = AppDomain.CurrentDomain.GetAssemblies()
+                                .SelectMany(assembly => assembly.GetTypes())
+                                .Where(type => type.IsSubclassOf(typeof(AbstractCharacterBuilder)))
+                                .ToList();
+
+            characters.ForEach(character =>
+            {
+                Console.WriteLine($"{character.ToString()}\n");
+            });
+        }
+
+        static bool KeepGoing()
+        {
+            Console.WriteLine("Press 1 if you want to build another character, or press anything else ");
+            var result = Console.ReadLine();
+
+            if (result == "1")
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
