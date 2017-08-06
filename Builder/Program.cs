c877d1b5e2bb70ec7cf2a3d39c9fa8e8edda330a
William Sanders <william.sanders.dev@gmail.com>
2017-08-06 19:18:37 -0400

    Print character builders alphabetically and with only builder name.

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -21,23 +21,50 @@ namespace Builder
             {
                 Console.WriteLine("Enter the number of the character that you want to build.");
 
-                PrintCharacters();
+                var (characterBuilders, characterNames) = GetCharacterBuildersAndNames();
+                PrintCharacters(characterNames);
 
                 keepLooping = KeepGoing();
             }            
         }
 
-        static void PrintCharacters()
+        static void PrintCharacters(List<string> names)
         {
+            var key = 1;
+            foreach (var name in names)
+            {
+                Console.WriteLine($"{key}. {name}");
+                key++;
+            }
+        }
+
+        static (Dictionary<int, Type>, List<string>) GetCharacterBuildersAndNames()
+        {
+            var characterBuilders = new Dictionary<int, Type>();
             var characters = AppDomain.CurrentDomain.GetAssemblies()
                                 .SelectMany(assembly => assembly.GetTypes())
                                 .Where(type => type.IsSubclassOf(typeof(AbstractCharacterBuilder)))
                                 .ToList();
 
+            var characterNames = new List<string>();
             characters.ForEach(character =>
             {
-                Console.WriteLine($"{character.ToString()}\n");
+                var nameArray = character.ToString().Split('.');
+                var name = nameArray[nameArray.Length - 1];
+                characterNames.Add(name);
             });
+
+            characterNames.Sort();
+
+            var key = 1;
+            characterNames.ForEach(name =>
+            {
+                var builder = characters.Where(x => x.ToString().Contains(name)).FirstOrDefault();
+                characterBuilders.Add(key, builder);
+                key++;
+            });
+
+            return (characterBuilders, characterNames);
         }
 
         static bool KeepGoing()
