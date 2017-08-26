860b4c20c1a3a178f9ad2fcd8ca2bbc201e44122
William Sanders <william.sanders076@gmail.com>
2017-08-26 18:31:19 -0400

    Add test project and unit tests for insertion sorter.

---
+++ SimpleFactoryTests/InsertionSorterUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using SimpleFactory;
+using System;
+using Xunit;
+
+namespace SimpleFactoryTests
+{
+    [Trait("Category", "Unit")]
+    public class InsertionSorterUnitTests
+    {
+        [Fact]
+        public void Sort_SortsCollection_WhenProvidedCollection()
+        {
+            const int arraySize = 1000;
+
+            var randomNumGenerator = new Random();
+            var arrayToSort = new int[arraySize];
+            for (int i = 0; i < arraySize; i++)
+            {
+                arrayToSort[i] = randomNumGenerator.Next(1, arraySize);
+            }
+
+            var sut = new InsertionSorter<int>();
+            sut.Sort(arrayToSort);
+
+            for (int i = 0; i < arraySize - 1; i++)
+            {
+                Assert.True(arrayToSort[i] <= arrayToSort[i + 1]);
+            }
+        }
+    }
+}
