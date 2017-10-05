eade154290adbe46b27a44b8622ded913dd6fa1f
William Sanders <william.sanders076@gmail.com>
2017-10-05 19:39:27 -0400

    Begin implementing client for bridge project.

--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Bridge.Converters;
+using CommonClientLib;
+using System;
 
 namespace Bridge
 {
     class Program
     {
+        private static TextParser TxtParser = new TextParser();
+        private static TypeParser TypParser = new TypeParser(TxtParser);
+        private static ContinuationDeterminer ContinuationDeterminer = new ContinuationDeterminer();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var keepLooping = true;
+
+            Console.WriteLine("**********************************************************************************************************");
+            Console.WriteLine("                  WELCOME TO THE BRIDGE PROGRAM -- WHICH IS A BORING PROGRAM THAT DOES CONVERSIONS");
+            Console.WriteLine("**********************************************************************************************************\n");
+
+            while (keepLooping)
+            {
+                var (converters, converterNames) = TypParser.GetTypeDictionaryAndNameList<AbstractConverter>();
+                Console.WriteLine("Enter the number of the measurement that you want to convert.");
+                TxtParser.PrintStringList(converterNames);
+
+
+            }
         }
     }
 }
--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,6 +1,5 @@
 ﻿using CommonClientLib;
 using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace Builder
