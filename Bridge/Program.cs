1ed20934fde6f5fc8053541ed9a70e7bbf833292
William Sanders <william.sanders076@gmail.com>
2017-10-08 10:14:04 -0400

    Bridge client instantiates converter with formatter based on user choice.

--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Bridge.Converters;
+using Bridge.Formatters;
 using CommonClientLib;
 using System;
 
@@ -9,13 +10,14 @@ namespace Bridge
         private static TextParser TxtParser = new TextParser();
         private static TypeParser TypParser = new TypeParser(TxtParser);
         private static ContinuationDeterminer ContinuationDeterminer = new ContinuationDeterminer();
+        private const string INVALID_CHOICE_MESSAGE = "You entered an invalid choice. I'm not mad; just disappointed. Let's try again I guess.";
 
         static void Main(string[] args)
         {
             var keepLooping = true;
 
             Console.WriteLine("**********************************************************************************************************");
-            Console.WriteLine("                  WELCOME TO THE BRIDGE PROGRAM -- WHICH IS A BORING PROGRAM THAT DOES CONVERSIONS");
+            Console.WriteLine("    WELCOME TO THE BRIDGE PROGRAM -- WHICH IS A BORING PROGRAM THAT SORT OF DOES CONVERSIONS");
             Console.WriteLine("**********************************************************************************************************\n");
 
             while (keepLooping)
@@ -24,12 +26,25 @@ namespace Bridge
                 Console.WriteLine("Enter the number of the measurement that you want to convert.");
                 TxtParser.PrintStringList(converterNames);
 
-                var strChoice = Console.ReadLine();
-                if (!TypParser.TryGetType(strChoice, converters, out var converter))
+                var strConverterChoice = Console.ReadLine();
+                if (!TypParser.TryGetType(strConverterChoice, converters, out var converter))
                 {
-                    Console.WriteLine("You entered an invalid choice. I'm not mad; just disappointed. Let's try again I guess.");
+                    Console.WriteLine(INVALID_CHOICE_MESSAGE);
                     continue;
                 }
+
+                var (formatters, formatterNames) = TypParser.GetInstantiatedTypeDictionaryAndNameList<IFormatter>();
+                Console.WriteLine("Enter the number of the way you want your conversion formatted.");
+                TxtParser.PrintStringList(formatterNames);
+
+                var strFormatterChoice = Console.ReadLine();
+                if (!TypParser.TryGetType(strFormatterChoice, formatters, out var formatter))
+                {
+                    Console.WriteLine(INVALID_CHOICE_MESSAGE);
+                    continue;
+                }
+
+                var instantiatedConverter = Activator.CreateInstance(converter, formatter);
 
                 keepLooping = ContinuationDeterminer.GoAgain();
             }
