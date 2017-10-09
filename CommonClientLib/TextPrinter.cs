8dd0d1cbba9e2df6fb9ef96d7cd49fe9077f12d6
William Sanders <william.sanders076@gmail.com>
2017-10-09 06:00:14 -0400

    Title printer returns console foreground color to whatever color it began at rather than only white.

--- CommonClientLib/TextPrinter.cs
+++ CommonClientLib/TextPrinter.cs
@@ -5,17 +5,18 @@ namespace CommonClientLib
     public class TextPrinter
     {
         /// <summary>
-        /// Prints the title of the application in green and centered. Changes the console foreground color to white on exit.
+        /// Prints the title of the application in green and centered.
         /// </summary>
         /// <param name="title">Application title.</param>
         public void PrintAppTitle(string title)
         {
+            var curColor = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine($"{new String('*', (Console.WindowWidth - 1))}");
             Console.Write(new string(' ', (Console.WindowWidth - title.Length) / 2));
             Console.WriteLine(title);
             Console.WriteLine($"{new String('*', (Console.WindowWidth - 1))}\n");
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = curColor;
         }
     }
 }
