2da0822473c3f83ffce07a9df860fb0f3fe21a5a
William Sanders <william.sanders076@gmail.com>
2017-10-14 22:05:02 -0400

    Add descendant and decedent classes.

---
+++ Composite/Decedent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Composite
+{
+    public class Decedent
+    {
+        public decimal EstateValue { get; set; }
+        public List<Descendant> Descendants { get; set; }
+    }
+}
---
+++ Composite/Descendant.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Composite
+{
+    public class Descendant
+    {
+        public string Name { get; set; }
+        public decimal Inheritance { get; set; }
+        public List<Descendant> Descendants { get; set; }
+    }
+}
