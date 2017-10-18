b24f09b50c6211bd89372b5504b7a69eb17590f7
William Sanders <william.sanders076@gmail.com>
2017-10-17 21:04:41 -0400

    Add unit tests for distribute estate method.

--- CompositeTests/DecedentUnitTests.cs
+++ CompositeTests/DecedentUnitTests.cs
@@ -45,5 +45,181 @@ namespace CompositeTests
 
             Assert.Equal(testDescendantOneInheritance, EXPECTED_INHERITANCE_TEST_DESCENDANT_ONE);
         }
+
+        [Fact]
+        public void DistributeEstate_DistributesPerStirpes_WhenDescendantHasDescendants()
+        {
+            const decimal TEST_ESTATE_VALUE = 90000;
+            const string TEST_DESCENDANT_ONE_NAME = "Bob";
+            const string TEST_DESCENDANT_TWO_NAME = "Ghent";
+            var testDescendants = new List<Descendant>
+            {
+                new Descendant
+                {
+                    Name = TEST_DESCENDANT_ONE_NAME,
+                    Deceased = false
+                },
+                new Descendant
+                {
+                    Name = "Charles",
+                    Deceased = false
+                },
+                new Descendant
+                {
+                    Name = "David",
+                    Deceased = true,
+                    Descendants = new List<Descendant>
+                    {
+                        new Descendant
+                        {
+                            Name = TEST_DESCENDANT_TWO_NAME,
+                            Deceased = false
+                        },
+                        new Descendant
+                        {
+                            Name = "Bevel",
+                            Deceased = false
+                        }
+                    }
+                }
+            };
+
+            const decimal EXPECTED_INHERITANCE_TEST_DESCENDANT_ONE = TEST_ESTATE_VALUE / 3;
+            const decimal EXPECTED_INHERITANCE_TEST_DESCENDANT_TWO =
+                EXPECTED_INHERITANCE_TEST_DESCENDANT_ONE / 2;
+
+            var sut = new Decedent(TEST_ESTATE_VALUE, testDescendants);
+
+            sut.DistributeEstate();
+
+            var testDescendantOneInheritance = testDescendants
+                .Where(d => d.Name == TEST_DESCENDANT_ONE_NAME)
+                .Select(d => d.Inheritance)
+                .FirstOrDefault();
+
+            var testDescendantTwoInheritance = testDescendants
+                .Where(d => d.Deceased)
+                .Select(d => d.Descendants)
+                .FirstOrDefault()
+                .Where(d => d.Name == TEST_DESCENDANT_TWO_NAME)
+                .Select(d => d.Inheritance)
+                .FirstOrDefault();
+
+            Assert.Equal(testDescendantOneInheritance, EXPECTED_INHERITANCE_TEST_DESCENDANT_ONE);
+            Assert.Equal(testDescendantTwoInheritance, EXPECTED_INHERITANCE_TEST_DESCENDANT_TWO);
+        }
+
+        [Fact]
+        public void DistributeEstate_DistributesPerStirpes_WhenDescendantOfDEscendantHasDescendants()
+        {
+            const decimal TEST_ESTATE_VALUE = 90000;
+            const string TEST_DESCENDANT_ONE_NAME = "Bob";
+            const string TEST_DESCENDANT_TWO_NAME = "Ghent";
+            const string TEST_DESCENDANT_THREE_NAME = "Bowlder";
+            const string TEST_DESCENDANT_FOUR_NAME = "David";
+            const string TEST_DESCENDANT_FIVE_NAME = "Flosser";
+            const string TEST_DESCENDANT_SIX_NAME = "Dimmerbergh";
+            var testDescendants = new List<Descendant>
+            {
+                new Descendant
+                {
+                    Name = TEST_DESCENDANT_FIVE_NAME,
+                    Deceased = true,
+                    Descendants = new List<Descendant>
+                    {
+                        new Descendant
+                        {
+                            Name = "Trovers",
+                            Deceased = false
+                        },
+                        new Descendant
+                        {
+                            Name = "Jaspers",
+                            Deceased = false
+                        },
+                        new Descendant
+                        {
+                            Name = TEST_DESCENDANT_SIX_NAME,
+                            Deceased = true,
+                            Descendants = new List<Descendant>
+                            {
+                                new Descendant
+                                {
+                                    Name = "Vlasssel",
+                                    Deceased = false,
+                                },
+                                new Descendant
+                                {
+                                    Name = TEST_DESCENDANT_THREE_NAME,
+                                    Deceased = false
+                                }
+                            }
+                        }
+                    }
+                },
+                new Descendant
+                {
+                    Name = TEST_DESCENDANT_ONE_NAME,
+                    Deceased = false
+                },
+                new Descendant
+                {
+                    Name = TEST_DESCENDANT_FOUR_NAME,
+                    Deceased = true,
+                    Descendants = new List<Descendant>
+                    {
+                        new Descendant
+                        {
+                            Name = TEST_DESCENDANT_TWO_NAME,
+                            Deceased = false
+                        },
+                        new Descendant
+                        {
+                            Name = "Bevel",
+                            Deceased = false
+                        }
+                    }
+                }
+            };
+
+            const decimal EXPECTED_INHERITANCE_TEST_DESCENDANT_ONE = TEST_ESTATE_VALUE / 3;
+            const decimal EXPECTED_INHERITANCE_TEST_DESCENDANT_TWO =
+                EXPECTED_INHERITANCE_TEST_DESCENDANT_ONE / 2;
+            const decimal EXPECTED_INHERITANCE_TEST_DESCENDANT_THREE =
+                ((TEST_ESTATE_VALUE / 3) / 3) / 2;
+
+            var sut = new Decedent(TEST_ESTATE_VALUE, testDescendants);
+
+            sut.DistributeEstate();
+
+            var testDescendantOneInheritance = testDescendants
+                .Where(d => d.Name == TEST_DESCENDANT_ONE_NAME)
+                .Select(d => d.Inheritance)
+                .FirstOrDefault();
+
+            var testDescendantTwoInheritance = testDescendants
+                .Where(d => d.Name == TEST_DESCENDANT_FOUR_NAME)
+                .Select(d => d.Descendants)
+                .FirstOrDefault()
+                .Where(d => d.Name == TEST_DESCENDANT_TWO_NAME)
+                .Select(d => d.Inheritance)
+                .FirstOrDefault();
+
+            var testDescendantThreeInheritance = testDescendants
+                .Where(d => d.Name == TEST_DESCENDANT_FIVE_NAME)
+                .Select(d => d.Descendants)
+                .FirstOrDefault()
+                .Where(d => d.Name == TEST_DESCENDANT_SIX_NAME)
+                .Select(d => d.Descendants)
+                .FirstOrDefault()
+                .Where(d => d.Name == TEST_DESCENDANT_THREE_NAME)
+                .Select(d => d.Inheritance)
+                .FirstOrDefault();
+
+
+            Assert.Equal(testDescendantOneInheritance, EXPECTED_INHERITANCE_TEST_DESCENDANT_ONE);
+            Assert.Equal(testDescendantTwoInheritance, EXPECTED_INHERITANCE_TEST_DESCENDANT_TWO);
+            Assert.Equal(testDescendantThreeInheritance, EXPECTED_INHERITANCE_TEST_DESCENDANT_THREE);
+        }
     }
 }
