2e342d02aa2894a64cd280666f96f32ac4eff651
William Sanders <william.sanders076@gmail.com>
2017-10-22 16:32:26 -0400

    Add QuestionAsker class to common library.

---
+++ CommonClientLib/QuestionAsker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CommonClientLib
+{
+    public class QuestionAsker
+    {
+        /// <summary>
+        /// Asks user whether the answer to a question is true or false.
+        /// </summary>
+        /// <param name="question">Question the user answers.</param>
+        /// <returns><code>true</code> if user enters 1; <code>false</code> if user enters 2.</returns>
+        public bool IsTrueOrFalse(string question)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{question} Enter 1 for true or 2 for false.");
+
+            while (true)
+            {
+                var answer = Console.ReadLine();
+                if (answer == "1")
+                {
+                    return true;
+                }
+                else if (answer == "2")
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("That input isn't valid. Let's try again.");
+                }
+            }
+        }
+    }
+}
