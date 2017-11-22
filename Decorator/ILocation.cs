1084e14a23cc07304d06d968036fd5b0bbb62508
William Sanders <william.sanders076@gmail.com>
2017-11-21 20:38:04 -0500

    Add decorator project

---
+++ Decorator/ILocation.cs
@@ -0,0 +1,8 @@
+﻿namespace Decorator
+{
+    public interface ILocation
+    {
+        string GetDescription();
+        int GetPopulation();
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,19 @@
+﻿using CommonClientLib;
+using System;
+
+namespace Decorator
+{
+    class Program
+    {
+        private static TextPrinter TxtPrinter = new TextPrinter();
+        private static TextParser TxtParser = new TextParser();
+        private static TypeParser TypParser = new TypeParser(TxtParser);
+        private static ContinuationDeterminer ContinuationDeterminer = new ContinuationDeterminer();
+        private static QuestionAsker QuestionAsker = new QuestionAsker();
+
+        static void Main(string[] args)
+        {
+            TxtPrinter.PrintInformation("WELCOME TO THE DECORATOR PROGRAM -- WHICH IS SORT OF COOL, I GUESS");
+        }
+    }
+}
