eeef659b2bd46606becb0579b020a2da1d77d292
William Sanders <william.sanders076@gmail.com>
2017-12-03 15:25:22 -0500

    Add TextToJson class

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -5,12 +5,14 @@ namespace Facade
     class Program
     {
         private static TextToXml TextToXml = new TextToXml();
+        private static TextToJson TextToJson = new TextToJson();
 
         static void Main(string[] args)
         {
-            var xDoxument = TextToXml.GetXmlFromText();
-            Console.WriteLine(xDoxument);
-
+            var jObj = TextToJson.GetJsonFromText();
+            //var xDoxument = TextToXml.GetXmlFromText();
+            //Console.WriteLine(xDoxument);
+            
             Console.ReadLine();
         }
     }
--- Facade/TextToJson.cs
+++ Facade/TextToJson.cs
@@ -1,12 +1,47 @@
 ﻿using Newtonsoft.Json.Linq;
+using CommonClientLib;
+using System.Collections.Generic;
 
 namespace Facade
 {
     public class TextToJson
     {
+        private QuestionAsker Asker = new QuestionAsker();
+        private FacadeCommonLib FacadeLib = new FacadeCommonLib();
+
+        /// <summary>
+        /// Gets a json tree based on information the user provides
+        /// </summary>
+        /// <returns><code>JObject</code> based on user input</returns>
         public JObject GetJsonFromText()
         {
-            return null;
+            var jObj = new JObject();
+
+            return jObj;
+        }
+
+        private void BuildJson(JObject jObj)
+        {
+            var firstQuestion = $"Is the value of the {jObj} a key-value pair, an object, or an array?";
+            var choices = new List<string>
+            {
+                "Key-value pair",
+                "Object",
+                "Array"
+            };
+
+            var firstChoice = choices[Asker.GetChoiceFromList(firstQuestion, choices)];
+            switch (firstChoice)
+            {
+                case "Key-value pair":
+                    break;
+                case "Object":
+                    break;
+                case "Array":
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
