f32727cd819f07893c0b81dd724f52e67c8da441
William Sanders <william.sanders076@gmail.com>
2018-02-03 16:49:38 -0500

    Add proxy project and DiskReader class

---
+++ Proxy/DiskReader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Proxy
+{
+    public class DiskReader : IDiskReader
+    {
+        public long NumFilesRead { get; set; }
+        public long NumBytesRead { get; set; }
+        public int NumDirectoriesNotSearched { get; set; }
+
+        public List<string> GetDrives()
+        {
+            return DriveInfo.GetDrives()
+                .Select(d => d.Name)
+                .ToList();
+        }
+
+        public List<string> GetFiles(string path, string extension = null)
+        {
+            NumFilesRead = 0;
+            NumBytesRead = 0;
+            NumDirectoriesNotSearched = 0;
+
+            List<string> getFiles(string _path, string _extension, List<string> _files)
+            {
+                try
+                {
+                    var _newFiles = _extension == null
+                        ? Directory.GetFiles(_path).ToList()
+                        : Directory.GetFiles(_path, _extension).ToList();
+
+                    foreach (var _file in _newFiles)
+                    {
+                        var _fileInfo = new FileInfo(_file);
+                        NumFilesRead++;
+                        NumBytesRead += _fileInfo.Length;
+                        _files.Add(_fileInfo.FullName);
+                    }
+
+                    Directory.GetDirectories(_path)
+                        .ToList()
+                        .ForEach(d => getFiles(d, _extension, _files));
+                }
+                catch (Exception ex)
+                {
+                    NumDirectoriesNotSearched++;
+                }
+
+                return _files;
+            }
+
+            return getFiles(path, extension, new List<string>());
+        }
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,63 @@
+﻿using CommonClientLib;
+using System;
+using System.Collections.Generic;
+
+namespace Proxy
+{
+    class Program
+    {
+        private static TextPrinter TxtPrinter = new TextPrinter();
+        private static QuestionAsker Asker = new QuestionAsker();
+        private static ContinuationDeterminer ContinuationDeterminer = new ContinuationDeterminer();
+        private static readonly List<string> YesOrNo = new List<string>
+        {
+            "Yes",
+            "No"
+        };
+
+        static void Main(string[] args)
+        {
+            TxtPrinter.PrintInformation("WELCOME TO THE PROXY PROGRAM -- WHICH PERFORMS MILDLY INTERESTING TASKS");
+
+            while (true)
+            {
+                var diskReader = new DiskReader();
+
+                var drives = diskReader.GetDrives();
+
+                var driveChoice = Asker.GetChoiceFromList("Choose a drive that you want to search: ", drives);
+
+                Console.WriteLine("What file extension do you want to search for?");
+                var extension = Console.ReadLine();
+
+                var files = diskReader.GetFiles(drives[driveChoice], extension);
+
+                if (files.Count == 0)
+                {
+                    Console.WriteLine($"Did not find any files with the extension {extension}");
+
+                    if (!ContinuationDeterminer.GoAgain())
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+
+                Console.WriteLine($"Found {files.Count} files with the extension {extension}.");
+                var printChoice = Asker.GetChoiceFromList("Do you want to see them?", YesOrNo);
+
+                if (YesOrNo[printChoice] == "Yes")
+                {
+                    foreach (var file in files)
+                    {
+                        Console.WriteLine(file);
+                    }
+                }
+
+                if (!ContinuationDeterminer.GoAgain())
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
