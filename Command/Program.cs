ce357af76f00889dcd44cf8f39ce4e8229e11515
William Sanders <william.sanders076@gmail.com>
2018-02-21 20:01:51 -0500

    Add command proj

---
+++ Command/Program.cs
@@ -0,0 +1,14 @@
+﻿using CommonClientLib;
+
+namespace Command
+{
+    class Program
+    {
+        private static TextPrinter TxtPrinter = new TextPrinter();
+
+        static void Main(string[] args)
+        {
+            TxtPrinter.PrintInformation("WELCOME TO THE COMMAND PROGRAM - WHICH IS YET FORMLESS");
+        }
+    }
+}
