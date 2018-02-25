91c194f469b8b921fde5c79a62d861d0871c1af4
William Sanders <william.sanders.dev@gmail.com>
2018-02-25 17:17:28 -0500

    Get command from user

--- Command/Program.cs
+++ Command/Program.cs
@@ -12,34 +12,38 @@ namespace Command
     {
         private static TextPrinter TxtPrinter = new TextPrinter();
         private static TypeParser TypeParser = new TypeParser(new TextParser());
+        private static QuestionAsker Asker = new QuestionAsker();
         private static ContinuationDeterminer ContinuationDeterminer = new ContinuationDeterminer();
         private const string ConfigurableItemsPath = "Items.json";
 
         static void Main(string[] args)
         {
             TxtPrinter.PrintInformation("WELCOME TO THE COMMAND PROGRAM - WHICH IS YET FORMLESS");
+            List<Item> items;
+            try
+            {
+                using (var reader = new StreamReader(ConfigurableItemsPath))
+                {
+                    var json = reader.ReadToEnd();
+                    items = JsonConvert.DeserializeObject<List<Item>>(json);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unable to retrieve item from '{ConfigurableItemsPath}'.\n" +
+                                  $"Exception message: {ex.Message}\n" +
+                                  $"Using default questions and answers instead.\n");
+
+                items = DefaultItems.GetDefaultItems();
+            }
+
+            var (commandDict, commandList) = TypeParser.GetInstantiatedTypeDictionaryAndNameList<ICommand>();
 
             while (true)
             {
-                List<Item> items;
-                try
-                {
-                    using (var reader = new StreamReader(ConfigurableItemsPath))
-                    {
-                        var json = reader.ReadToEnd();
-                        items = JsonConvert.DeserializeObject<List<Item>>(json);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Unable to retrieve item from '{ConfigurableItemsPath}'.\n" +
-                                      $"Exception message: {ex.Message}\n" +
-                                      $"Using default questions and answers instead.\n");
+                var commandChoice = Asker.GetChoiceFromList("What do you want to do with your order?", commandList) + 1;
 
-                    items = DefaultItems.GetDefaultItems();
-                }
-
-                var (commandDict, commandList) = TypeParser.GetInstantiatedTypeDictionaryAndNameList<ICommand>();
+                Console.WriteLine($"You chose the {commandDict[commandChoice].Description} command.");
 
                 if (!ContinuationDeterminer.GoAgain())
                 {
