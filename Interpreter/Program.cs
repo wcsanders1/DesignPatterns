bd1124e7d0077b5d02808e1f94482d8197544e72
William Sanders <william.sanders076@gmail.com>
2018-03-04 16:49:30 -0500

    Scaffold interpreter project

---
+++ Interpreter/Program.cs
@@ -0,0 +1,14 @@
+﻿using CommonClientLib;
+
+namespace Interpreter
+{
+    class Program
+    {
+        private static TextPrinter TxtPrinter = new TextPrinter();
+
+        static void Main(string[] args)
+        {
+            TxtPrinter.PrintInformation("WELCOME TO THE INTERPRETER PROGRAM -- WHICH CAN BE NEAT TO USE");
+        }
+    }
+}
