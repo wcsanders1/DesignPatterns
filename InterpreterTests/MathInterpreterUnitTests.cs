311bac445eb9c1172aafebf9bbef7e00b69f2a86
William Sanders <william.sanders076@gmail.com>
2018-03-10 14:27:08 -0500

    Add unit tests for MathInterpreter

--- InterpreterTests/MathInterpreterUnitTests.cs
+++ InterpreterTests/MathInterpreterUnitTests.cs
@@ -53,6 +53,78 @@ namespace InterpreterTests
             Assert.Equal(expectedAnswer, result);
         }
 
+        [Fact]
+        public void GetAnswer_ReturnsCorrectAnswer_WhenProvidedSimpleSubtractionExpression()
+        {
+            const string testExpression = "8 - 3";
+            const decimal expectedAnswer = 5;
+
+            var sut = new MathInterpreter();
+            var result = sut.GetAnswer(testExpression);
+
+            Assert.Equal(expectedAnswer, result);
+        }
+
+        [Fact]
+        public void GetAnswer_ReturnsCorrectAnswer_WhenProvidedLargeSubtractionExpression()
+        {
+            const string testExpression = "8854201 - 33445";
+            const decimal expectedAnswer = 8820756;
+
+            var sut = new MathInterpreter();
+            var result = sut.GetAnswer(testExpression);
+
+            Assert.Equal(expectedAnswer, result);
+        }
+
+        [Fact]
+        public void GetAnswer_ReturnsCorrectAnswer_WhenProvidedThreePartSubtractionExpression()
+        {
+            const string testExpression = "8854201 - 33445 - 54";
+            const decimal expectedAnswer = 8820702;
+
+            var sut = new MathInterpreter();
+            var result = sut.GetAnswer(testExpression);
+
+            Assert.Equal(expectedAnswer, result);
+        }
+
+        [Fact]
+        public void GetAnswer_ReturnsCorrectAnswer_WhenProvidedNegativeResultSubtractionExpression()
+        {
+            const string testExpression = "33445 - 8854201";
+            const decimal expectedAnswer = -8820756;
+
+            var sut = new MathInterpreter();
+            var result = sut.GetAnswer(testExpression);
+
+            Assert.Equal(expectedAnswer, result);
+        }
+
+        [Fact]
+        public void GetAnswer_ReturnsCorrectAnswer_WhenProvidedExpressionWithAdditionAndSubtractionExpression()
+        {
+            const string testExpression = "8854201 + 4435 - 690";
+            const decimal expectedAnswer = 8857946;
+
+            var sut = new MathInterpreter();
+            var result = sut.GetAnswer(testExpression);
+
+            Assert.Equal(expectedAnswer, result);
+        }
+
+        [Fact]
+        public void GetAnswer_ReturnsCorrectAnswer_WhenProvidedComplexAdditionAndSubstrationExpression()
+        {
+            const string testExpression = "8854201 + 4435 - 690 - 77744655 + 1 + 1+234   + 656432 - 90 - 90-99+1 +590333";
+            const decimal expectedAnswer = -67639986;
+
+            var sut = new MathInterpreter();
+            var result = sut.GetAnswer(testExpression);
+
+            Assert.Equal(expectedAnswer, result);
+        }
+
         //[Fact]
         //public void GetAnswer_ReturnsCorrectAnswer_WhenProvidedExpressionWithDecimal_4()
         //{
