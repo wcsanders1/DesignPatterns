4e098160ef17f903b04fc437e954ce05baac3b30
William Sanders <william.sanders076@gmail.com>
2018-03-30 06:03:41 -0400

    Scaffold Iterator project

---
+++ Iterator/Program.cs
@@ -0,0 +1,25 @@
+﻿using CommonClientLib;
+using System;
+
+namespace Iterator
+{
+    class Program
+    {
+        private static TextPrinter TxtPrinter = new TextPrinter();
+        private static ContinuationDeterminer ContinuationDeterminer = new ContinuationDeterminer();
+
+        static void Main(string[] args)
+        {
+            TxtPrinter.PrintInformation("WELCOME TO THE ITERATOR PROGRAM -- WHICH IS A BORING PROGRAM TO USE");
+
+            while (true)
+            {
+
+                if (!ContinuationDeterminer.GoAgain())
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
