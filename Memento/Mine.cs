70b8f4391b667fe6da7dd14135ea935411f73d74
William Sanders <william.sanders076@gmail.com>
2018-04-24 06:32:58 -0400

    Mine class creates mine board

--- Memento/Mine.cs
+++ Memento/Mine.cs
@@ -1,15 +1,44 @@
-﻿namespace Memento
+﻿using System;
+
+namespace Memento
 {
     public class Mine
     {
         private int Width { get; }
         private int Height { get; }
-        private MineSpace[][] MineBoard {get;}
+        private int MinVal { get; }
+        private int MaxVal { get; }
+        private MineSpace[,] MineBoard { get; }
 
-        public Mine(int width, int height)
+        public Mine(int width, int height, int minVal, int maxVal)
         {
             Width = width;
             Height = height;
+            MinVal = minVal;
+            MaxVal = maxVal;
+            MineBoard = new MineSpace[Height, Width];
+        }
+
+        public void PrintMineBoard()
+        {
+            if (MineBoard == null)
+            {
+                CreateMindBoard();
+            }
+        }
+
+        private void CreateMindBoard()
+        {
+            var rnd = new Random();
+
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    var mineSpace = new MineSpace { ExplosionValue = rnd.Next(MinVal, MaxVal) };
+                    MineBoard[y, x] = mineSpace;
+                }
+            }
         }
 
         public IMineMemento CreateMemento()
