c6d9ed841a638eb4f29d5e2acddaf6698d3fb95f
William Sanders <william.sanders076@gmail.com>
2018-05-05 15:38:26 -0400

    Scaffold observer project

---
+++ Observer/Program.cs
@@ -0,0 +1,24 @@
+﻿using CommonClientLib;
+using System;
+
+namespace Observer
+{
+    class Program
+    {
+        private static TextPrinter TxtPrinter = new TextPrinter();
+        private static ContinuationDeterminer ContinuationDeterminer = new ContinuationDeterminer();
+
+        static void Main(string[] args)
+        {
+            TxtPrinter.PrintInformation("WELCOME TO THE OBSERVER PROGRAM -- WHICH IS SOMEWHAT FUNNY");
+
+            while (true)
+            {
+                if (!ContinuationDeterminer.GoAgain())
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
