ace5dc11d3ecc689d50e7a88b0dfd1541e91287a
William Sanders <william.sanders076@gmail.com>
2018-06-09 14:17:00 -0400

    Replace program class after having accidentally deleted it

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using CommonClientLib;
+using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        private static TextPrinter TxtPrinter = new TextPrinter();        
+        private static ContinuationDeterminer ContinuationDeterminer = new ContinuationDeterminer();
+        private static QuestionAsker Asker = new QuestionAsker();
+
+        static void Main(string[] args)
+        {
+            TxtPrinter.PrintInformation("WELCOME TO THE INTERPRETER METHOD PROGRAM -- WHICH MAY PROVIDE SOME INTEREST");
+
+            while (true)
+            {
+                if (!ContinuationDeterminer.GoAgain())
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
